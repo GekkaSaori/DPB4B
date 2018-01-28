f3af9116496364d7803406949ba393278c60befc
MingqiHuang <hmq011212@163.com>
2018-01-28 18:07:31 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 mkdir tmp
 xcopy src tmp /E /Q
 cd tmp
-latexmk -f -lualatex --shell-escape %~dp0/tmp/DP.tex >> ..\logs.txt
+latexmk -f -lualatex --shell-escape %~dp0/tmp/DP.tex >> ..\logs.log
 copy DP.pdf ..
 del *.* /F /S /Q >NUL
 cd ..
