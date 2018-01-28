f9707e80e5e3a88facd0eab2e47077da1623b1fc
MingqiHuang <hmq011212@163.com>
2018-01-28 18:00:43 +0800

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+mkdir tmp
+xcopy src tmp /E /Q
+cd tmp
+latexmk -f -lualatex --shell-escape %~dp0/tmp/DP.tex >> ..\logs.txt
+copy DP.pdf ..
+del *.* /F /S /Q >NUL
+cd ..
+rd tmp /S /Q >NUL
+pause
