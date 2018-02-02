b45588ac5376fcba82a1d451201e0c1ef33d3908
MingqiHuang <hmq011212@163.com>
2018-02-02 13:49:47 +0800

    rewrite the script as clean build tool.

---
+++ clean-build.bat
@@ -0,0 +1,16 @@
+@echo off
+if exist DP.pdf @del DP.pdf /F /S /Q >NUL
+if exist logs.log @del logs.log /F /S /Q >NUL
+if exist tmp (cd tmp
+@del *.* /F /S /Q >NUL
+cd ..
+@rd tmp /S /Q >NUL)
+mkdir tmp
+@xcopy src tmp /E /Q /C >NUL
+cd tmp
+latexmk -f -lualatex --shell-escape %~dp0/tmp/DP.tex >> ..\logs.log
+@copy DP.pdf ..
+@del *.* /F /S /Q >NUL
+cd ..
+@rd tmp /S /Q >NUL
+pause
